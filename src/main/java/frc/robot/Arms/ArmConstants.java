ba79c8e8cfe6c4ddbfba459681368150b0e475c9
Rob <rob@mymail.com>
2024-03-09 12:11:50 -0800

    Some sample Falcon code for climbing

--- src/main/java/frc/robot/Arms/ArmConstants.java
+++ src/main/java/frc/robot/Arms/ArmConstants.java
@@ -1,17 +1,213 @@
 /* 
+ * Copyright (C) Cross The Road Electronics.  All rights reserved.
+ * License information can be found in CTRE_LICENSE.txt
+ * For support and suggestions contact support@ctr-electronics.com or file
+ * an issue tracker at https://github.com/CrossTheRoadElec/Phoenix-Releases
+ */
 package frc.robot.Arms;
-
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.ClosedLoopOutputType;
 
-public final class ArmConstants {
-public static class ArmOperatorConstants {
-    public static final int kArm_1_ID = 30;
-    public static final int kArm_2_ID = 31;
+/**
+ * All constants for a swerve module.
+ */
+public class ArmConstants {
 
-    public static final int ArmControllerPort = 1;
+    public enum ArmClosedLoopOutputType {
+        Voltage,
+        /** Requires Pro */
+        TorqueCurrentFOC,
+    }
+    /** CAN ID of the drive motor. */
+    public int DriveMotorId = 0;
+
+    /** Gear ratio between the drive motor and the wheel. */
+    public double DriveMotorGearRatio = 0;
+    /** Radius of the driving wheel in inches. */
+    public double WheelRadius = 0;
+    /**
+     * The location of this module's wheels relative to the physical center of the robot in meters
+     * along the X axis of the robot.
+     */
+    public double LocationX = 0;
+    /**
+     * The location of this module's wheels relative to the physical center of the robot in meters
+     * along the Y axis of the robot.
+     */
+    public double LocationY = 0;
+
+    /**
+     * The drive motor closed-loop gains.
+     * <p>
+     * When using closed-loop control, the drive motor uses the control output
+     * type specified by {@link #DriveMotorClosedLoopOutput} and any closed-loop
+     * {@link SwerveModule.DriveRequestType}.
+     */
+    public Slot0Configs DriveMotorGains = new Slot0Configs();
+
+    /** The closed-loop output type to use for the drive motors. */
+    public ArmClosedLoopOutputType DriveMotorClosedLoopOutput = ArmClosedLoopOutputType.Voltage;
+
+    /** The maximum amount of stator current the drive motors can apply without slippage. */
+    public double SlipCurrent = 400;
+
+    /** True if the driving motor is reversed. */
+    public boolean DriveMotorInverted = false;
+
+    /**
+     * When using open-loop drive control, this specifies the speed at which the robot travels
+     * when driven with 12 volts, in meters per second. This is used to approximate the output
+     * for a desired velocity. If using closed loop control, this value is ignored.
+     */
+    public double SpeedAt12VoltsMps = 0;
+
+
+    /** Sim-specific constants **/
+    /** Simulated drive inertia in kilogram meters squared. */
+    public double DriveInertia = 0.001;
+    /** Simulated drive voltage required to overcome friction. */
+    public double DriveFrictionVoltage = 0.25;
+
+    /**
+     * Sets the CAN ID of the drive motor.
+     *
+     * @param id CAN ID of the drive motor
+     * @return this object
+     */
+    public ArmConstants withDriveMotorId(int id) {
+        this.DriveMotorId = id;
+        return this;
+    }
+    /**
+     * Sets the gear ratio between the drive motor and the wheel.
+     *
+     * @param ratio Gear ratio between the drive motor and the wheel
+     * @return this object
+     */
+    public ArmConstants withDriveMotorGearRatio(double ratio) {
+        this.DriveMotorGearRatio = ratio;
+        return this;
+    }
+
+    /**
+     * Sets the radius of the driving wheel in inches.
+     *
+     * @param radius Radius of the driving wheel in inches
+     * @return this object
+     */
+    public ArmConstants withWheelRadius(double radius) {
+        this.WheelRadius = radius;
+        return this;
+    }
+
+    /**
+     * Sets the location of this module's wheels relative to the physical center of the robot in
+     * meters along the X axis of the robot.
+     *
+     * @param locationXMeters Location of this module's wheels
+     * @return this object
+     */
+    public ArmConstants withLocationX(double locationXMeters) {
+        this.LocationX = locationXMeters;
+        return this;
+    }
+
+    /**
+     * Sets the location of this module's wheels relative to the physical center of the robot in
+     * meters along the Y axis of the robot.
+     *
+     * @param locationYMeters Location of this module's wheels
+     * @return this object
+     */
+    public ArmConstants withLocationY(double locationYMeters) {
+        this.LocationY = locationYMeters;
+        return this;
+    }
+
+    /**
+     * Sets the drive motor closed-loop gains.
+     * <p>
+     * When using closed-loop control, the drive motor uses the control output
+     * type specified by {@link #DriveMotorClosedLoopOutput} and any closed-loop
+     * {@link SwerveModule.DriveRequestType}.
+     *
+     * @param gains Drive motor closed-loop gains
+     * @return this object
+     */
+    public ArmConstants withDriveMotorGains(Slot0Configs gains) {
+        this.DriveMotorGains = gains;
+        return this;
+    }
+
+    /**
+     * Sets closed-loop output type to use for the drive motors.
+     *
+     * @param outputType Closed-loop output type to use for the drive motors
+     * @return this object
+     */
+    public ArmConstants withDriveMotorClosedLoopOutput(ArmClosedLoopOutputType outputType) {
+        this.DriveMotorClosedLoopOutput = outputType;
+        return this;
+    }
+
+    /**
+     * Sets the maximum amount of stator current the drive motors can
+     * apply without slippage.
+     *
+     * @param slipCurrent Maximum amount of stator current
+     * @return this object
+     */
+    public ArmConstants withSlipCurrent(double slipCurrent) {
+        this.SlipCurrent = slipCurrent;
+        return this;
+    }
+
+    /**
+     * Sets whether the driving motor is reversed.
+     *
+     * @param driveMotorInverted True if the driving motor is reversed
+     * @return this object
+     */
+    public ArmConstants withDriveMotorInverted(boolean driveMotorInverted) {
+        this.DriveMotorInverted = driveMotorInverted;
+        return this;
+    }
+
+    /**
+     * When using open-loop drive control, this specifies the speed at which the robot travels
+     * when driven with 12 volts, in meters per second. This is used to approximate the output
+     * for a desired velocity. If using closed loop control, this value is ignored.
+     *
+     * @param speedAt12VoltsMps Speed at which the robot travels when driven with
+     *                          12 volts, in meters per second
+     * @return this object
+     */
+    public ArmConstants withSpeedAt12VoltsMps(double speedAt12VoltsMps) {
+        this.SpeedAt12VoltsMps = speedAt12VoltsMps;
+        return this;
+    }
+
+    /**
+     * Sets the simulated drive inertia in kilogram meters squared.
+     *
+     * @param driveInertia Drive inertia in kilogram meters squared
+     * @return this object
+     */
+    public ArmConstants withDriveInertia(double driveInertia) {
+        this.DriveInertia = driveInertia;
+        return this;
+    }
+
+    /**
+     * Sets the simulated drive voltage required to overcome friction.
+     *
+     * @param voltage Drive voltage required to overcome friction
+     * @return this object
+     */
+    public ArmConstants withDriveFrictionVoltage(double voltage) {
+        this.DriveFrictionVoltage = voltage;
+        return this;
+    }
 }
-}
-*/
-
--- src/main/java/frc/robot/Arms/ArmContainer.java
+++ src/main/java/frc/robot/Arms/ArmContainer.java
@@ -1,33 +1,134 @@
-/* 
+/*
+ * Copyright (C) Cross The Road Electronics.  All rights reserved.
+ * License information can be found in CTRE_LICENSE.txt
+ * For support and suggestions contact support@ctr-electronics.com or file
+ * an issue tracker at https://github.com/CrossTheRoadElec/Phoenix-Releases
+ */
 package frc.robot.Arms;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TorqueCurrentConfigs;
+import com.ctre.phoenix6.controls.MotionMagicExpoTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.MotionMagicExpoVoltage;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import frc.robot.Arms.ArmConstants;
-import frc.robot.Arms.ArmConstants.ArmOperatorConstants;
 
+/**
+ * Swerve Module class that encapsulates a swerve module powered by CTR
+ * Electronics devices.
+ * <p>
+ * This class handles the hardware devices and configures them for
+ * swerve module operation using the Phoenix 6 API.
+ * <p>
+ * This class constructs hardware devices internally, so the user
+ * only specifies the constants (IDs, PID gains, gear ratios, etc).
+ * Getters for these hardware devices are available.
+ */
 public class ArmContainer {
+    /**
+     * All possible control requests for the module drive motor.
+     */
+    public enum DriveRequestType {
+        /**
+         * Control the drive motor using an open-loop voltage request.
+         */
+        OpenLoopVoltage,
+        /**
+         * Control the drive motor using a velocity closed-loop request.
+         * The control output type is determined by {@link ArmConstants#DriveMotorClosedLoopOutput}
+         */
+        Velocity,
+    }
 
-   
+    private final TalonFX m_driveMotor;
 
-    private final Arm arms = new Arm();
+    private final StatusSignal<Double> m_drivePosition;
+    private final StatusSignal<Double> m_driveVelocity;
+    private final BaseStatusSignal[] m_signals;
+    private final double m_driveRotationsPerMeter;
 
-    
-    private final CommandXboxController m_armController =
-      new CommandXboxController(ArmOperatorConstants.ArmControllerPort);
+    private final double m_speedAt12VoltsMps;
+
+    /* drive motor controls */
+    private final VoltageOut m_voltageOpenLoopSetter = new VoltageOut(0);
+    private final VelocityVoltage m_velocityVoltageSetter = new VelocityVoltage(0);
+    /* Velocity Torque current neutral should always be coast, as neutral corresponds to 0-current or maintain velocity, not 0-velocity */
+    private final VelocityTorqueCurrentFOC m_velocityTorqueSetter = new VelocityTorqueCurrentFOC(0).withOverrideCoastDurNeutral(true);
+
+    private final ArmConstants.ArmClosedLoopOutputType m_driveClosedLoopOutput;
+
+    /**
+     * Construct a ArmContainer with the specified constants.
+     *
+     * @param constants   Constants used to construct the module
+     * @param canbusName  The name of the CAN bus this module is on
+     */
+    public ArmContainer(ArmConstants constants, String canbusName) {
+        m_driveMotor = new TalonFX(constants.DriveMotorId, canbusName);
+
+        TalonFXConfiguration talonConfigs = new TalonFXConfiguration();
+
+        talonConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+        talonConfigs.Slot0 = constants.DriveMotorGains;
+        talonConfigs.TorqueCurrent.PeakForwardTorqueCurrent = constants.SlipCurrent;
+        talonConfigs.TorqueCurrent.PeakReverseTorqueCurrent = -constants.SlipCurrent;
+        talonConfigs.CurrentLimits.StatorCurrentLimit = constants.SlipCurrent;
+        talonConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+
+        talonConfigs.MotorOutput.Inverted = constants.DriveMotorInverted ? InvertedValue.Clockwise_Positive
+                : InvertedValue.CounterClockwise_Positive;
+        StatusCode response = m_driveMotor.getConfigurator().apply(talonConfigs);
+        if (!response.isOK()) {
+            System.out.println(
+                    "TalonFX ID " + m_driveMotor.getDeviceID() + " failed config with error " + response.toString());
+        }
+
+        /* Undo changes for torqueCurrent */
+        talonConfigs.TorqueCurrent = new TorqueCurrentConfigs();
+        /* And to current limits */
+        talonConfigs.CurrentLimits = new CurrentLimitsConfigs();
+
+        talonConfigs.ClosedLoopGeneral.ContinuousWrap = true; // Enable continuous wrap for swerve modules
+
+        m_drivePosition = m_driveMotor.getPosition().clone();
+        m_driveVelocity = m_driveMotor.getVelocity().clone();
+
+        m_signals = new BaseStatusSignal[4];
+        m_signals[0] = m_drivePosition;
+        m_signals[1] = m_driveVelocity;
+
+        /* Calculate the ratio of drive motor rotation to meter on ground */
+        double rotationsPerWheelRotation = constants.DriveMotorGearRatio;
+        double metersPerWheelRotation = 2 * Math.PI * Units.inchesToMeters(constants.WheelRadius);
+        m_driveRotationsPerMeter = rotationsPerWheelRotation / metersPerWheelRotation;
+
+        m_velocityTorqueSetter.UpdateFreqHz = 0;
+        m_velocityVoltageSetter.UpdateFreqHz = 0;
+        m_voltageOpenLoopSetter.UpdateFreqHz = 0;
+
+        /* Set the drive motor closed-loop output type */
+        m_driveClosedLoopOutput = constants.DriveMotorClosedLoopOutput;
+
+        /* Get the expected speed when applying 12 volts */
+        m_speedAt12VoltsMps = constants.SpeedAt12VoltsMps;
+    }
 
 }
-
-public ArmContainer() {
-    configureBindings();
-}
-
-private void configureBindings() {
-    m_armController
-    .()
-    .whileTrue(
-        new Climb(arms));
-}
-*/
