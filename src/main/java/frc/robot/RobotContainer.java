03de60ca3b31d1694b855c3b6b1445619bad971e
CosimoAho <c3000funnypenguin@icloud.com>
2024-03-09 16:28:46 -0800

    Added basic version of climer code, thanks to Team Toast

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/* Speed  */
+
 package frc.robot;
 
 import frc.robot.Shooter.ShooterContainer;
@@ -24,26 +26,30 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.generated.TunerConstants; 
+import com.ctre.phoenix6.hardware.TalonFX;
 
 //import frc.robot.Arms.ArmContainer;
 
 public class RobotContainer {
   private final Joystick Rdriver = new Joystick(1);
   private final Joystick Ldriver = new Joystick(0); 
+  private final TalonFX lClimber = new TalonFX(31);
+  private final TalonFX rClimber = new TalonFX(30);
 
   private double MaxSpeed = TunerConstants.kSpeedAt12VoltsMps; // kSpeedAt12VoltsMps desired top speed
   private double MaxAngularRate = 1.25 * Math.PI; // 3/4 of a rotation per second max angular velocity
 
   /* Setting up bindings for necessary control of the swerve drive platform */
-  private final CommandXboxController lJoystick = new CommandXboxController(0); // My joystick
+  public final CommandXboxController lJoystick = new CommandXboxController(0); // My joystick
   //private final CommandXboxController Joystick_2 = new CommandXboxController(1); // creates another instance of joystick for when two are plugged in.
   private final CommandSwerveDrivetrain drivetrain = TunerConstants.DriveTrain; // My drivetrain
 
-  private Command runAuto1 = drivetrain.getAutoPath("middle");
-  private Command runAuto2 = drivetrain.getAutoPath("right");
-  private Command runAuto3 = drivetrain.getAutoPath("left");
+  private Command runAuto1 = drivetrain.getAutoPath("middle path");
+  private Command runAuto2 = drivetrain.getAutoPath("right path");
+  private Command runAuto3 = drivetrain.getAutoPath("left path");
 
   private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
       .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.50) // Add a 10% deadband
@@ -53,13 +59,12 @@ public class RobotContainer {
   private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
   private final Telemetry logger = new Telemetry(MaxSpeed);
 
-  public static final double kDSpeedDiv =  1.0; // Value for controlling controller sensitivity
-  public static final double kAAngleDiv =  0.25; // Value for controlling controller sensitivity
+  public static final double kDSpeedDiv =  1.3; // Value for controlling controller drive axis (top stick) sensitivity
+  public static final double kAAngleDiv =  0.25; // Value for controlling controller angle axis (bottom stick0) sensitivity
 
   private void configureBindings() {
     drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
         drivetrain.applyRequest(()  ->  {
-          System.out.println("joystick.getRightX=" + -lJoystick.getRightX() );
            return drive.withVelocityX(-lJoystick.getLeftY() * MaxSpeed/kDSpeedDiv) // Drive forward with
                                                                                            // negative Y (forward)
             .withVelocityY(-lJoystick.getLeftX() * MaxSpeed/kDSpeedDiv) // Drive left with negative X (left)
@@ -71,8 +76,8 @@ public class RobotContainer {
     //joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
      //lJoystick.b().whileTrue(drivetrain
        // .applyRequest(() -> point.withModuleDirection(new Rotation2d(-lJoystick.getLeftY(), -lJoystick.getLeftX()))));
-      lJoystick.x().whileTrue(drivetrain.applyRequest(() -> drive.withRotationalRate(MaxAngularRate)));
-      lJoystick.y().whileTrue(drivetrain.applyRequest(() -> drive.withRotationalRate(MaxAngularRate * -1)));
+      // lJoystick.x().whileTrue(drivetrain.applyRequest(() -> drive.withRotationalRate(MaxAngularRate)));
+      // lJoystick.y().whileTrue(drivetrain.applyRequest(() -> drive.withRotationalRate(MaxAngularRate * -1)));
 
     // reset the field-centric heading on left bumper press
     //joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldRelative()));
@@ -81,6 +86,33 @@ public class RobotContainer {
       drivetrain.seedFieldRelative(new Pose2d(new Translation2d(), Rotation2d.fromDegrees(90)));
     }
     drivetrain.registerTelemetry(logger::telemeterize);
+
+
+    lJoystick.x().onTrue(
+      new InstantCommand(() -> {
+        lClimber.set(0.5);
+        rClimber.set(0.5);
+      })
+    );
+    lJoystick.x().onFalse(
+      new InstantCommand(() -> {
+        lClimber.set(0);
+        rClimber.set(0);
+      })
+    );
+    lJoystick.y().onTrue(
+      new InstantCommand(() -> {
+        lClimber.set(-0.5);
+        rClimber.set(-0.5);
+      })
+    );
+
+    lJoystick.y().onFalse(
+      new InstantCommand(() -> {
+        lClimber.set(0);
+        rClimber.set(0);
+      })
+    );
   }
 
 
@@ -89,7 +121,6 @@ public class RobotContainer {
   }
  
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-    //return runAuto1;
+    return runAuto1;
   }
 }
